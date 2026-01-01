170918b590c3a7fbce1a16713d0e26760263596e
= <144963802+milesdripps@users.noreply.github.com>
2026-01-01 11:01:09 -0500

    basic implementation. needs bug fixing

---
+++ main.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <future>
+#include <queue>
+
+class ThreadPool{
+    public:
+    ThreadPool(size_t n) :   number_of_threads(n){
+        threads_.reserve(n);
+    }
+    
+    //TODO: Implement concurrent enqueueing.
+    void enqueue(const std::function<void()>&& func){
+        if(num_jobs < 0){
+            std::cout << "Negative job count, potential data race\n";
+        }
+        
+        num_jobs++;
+        tasks.emplace(func);
+        
+        if(num_jobs == 1 && running){
+            //only 1 thread will be awaiting this.
+            jobs_available_condition_var.notify_one();
+        }
+
+        std::cout << "Function queued. job count: " << num_jobs << std::endl;
+    }
+
+    void start(){ 
+        running = true;
+        std::cout << "Started\n";
+        run();
+    }
+
+    void stop(){
+        std::cout << "Stopping...\n";
+        running = false;
+        dequeue_condition_var.notify_all();
+        jobs_available_condition_var.notify_all();
+
+    }
+
+    private:
+//------- Thread/vector members
+        size_t number_of_threads;
+        std::vector<std::thread> threads_;
+        std::queue<std::function<void()>> tasks;
+        size_t num_jobs = 0;
+//------- Atomic/concurrency members
+        bool running = false;
+        bool dequeue_open = true;
+        std::mutex dequeue_mutex;
+        std::mutex job_mutex;
+        std::condition_variable dequeue_condition_var;
+        std::condition_variable jobs_available_condition_var;
+
+        static void stopping(){
+            std::cout << "Thread " << std::this_thread::get_id() << " Stopping\n";
+        }
+
+        auto call(){
+            return this->stopping();
+        }
+
+//------- Retreive job from queue
+        bool dequeue(std::function<void()>& work_function) {
+            std::unique_lock<std::mutex> lock(dequeue_mutex);
+            std::unique_lock<std::mutex> job_count_lock(job_mutex); 
+            std::cout << "Thread "<< std::this_thread::get_id() << " Has locked dequeue()\n";
+
+            dequeue_condition_var.wait(lock, [this](){ // essentially a better lock.lock();
+                 return !running || dequeue_open; // re-checks this upon 
+            });   
+
+            jobs_available_condition_var.wait(job_count_lock, [this](){
+                return !running || (num_jobs > 0);
+            });
+
+            if(!running){
+                return false;
+            }
+
+            
+            //do work
+            std::cout << "doing work\n";
+            dequeue_open = false;
+            work_function = std::move(tasks.front());
+            tasks.pop();
+            num_jobs--;
+
+            // let another thread access now
+            dequeue_open = true;
+            lock.unlock();
+            job_count_lock.unlock();
+            dequeue_condition_var.notify_one();
+
+            std::cout << "returning\n";
+            return true;
+        }
+//------ Run the thread pool
+        void run(){
+            for(int i = 0; i < number_of_threads; i++){
+                threads_.emplace_back(std::thread([this, i](){
+                    std::cout << "Thread " << i << " Created\n";
+                    while(running){
+                        std::cout << "Thread " << i << " Awaiting work\n"; 
+                        //get a function, do its work.
+                        std::function<void()> work;
+                        if(dequeue(work)){ work(); }
+                    }
+                    std::cout << "Thread "<<i<<" Stopped\n";
+                }));
+            }
+
+            // for(auto& th : threads_){
+            //     th.join();
+            // }
+        }
+
+    
+
+
+};
+
+void func1(){
+    std::cout << "Function 1 Printing!\n";
+}
+void func2(){
+    std::cout << "Function 2 Printing!\n";
+}
+void func3(){
+    std::cout << "Function 3 Printing!\n";
+}
+void func4(){
+    std::cout << "Function 4 Printing!\n";
+}
+void func5(){
+    std::cout << "Function 5 Printing!\n";
+}
+void func6(){
+    std::cout << "Function 6 Printing!\n";
+}
+
+int main(){
+    std::cout << "Thread Pool\n";
+    // tests
+    ThreadPool pool(1);
+    pool.enqueue(std::move(func1));
+    pool.enqueue(std::move(func2));
+    pool.enqueue(std::move(func3));
+    pool.enqueue(std::move(func4));
+    pool.enqueue(std::move(func5));
+    pool.enqueue(std::move(func6));
+    pool.enqueue([](){
+        std::cout << "Lambda Fn Printing!\n";
+    });
+    pool.start();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    pool.stop();
+    std::cin.get();
+    return 0;
+}
